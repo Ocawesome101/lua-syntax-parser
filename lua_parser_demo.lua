eaa4e07a74972fb9e97865696264d4603b698757
Ocawesome101 <ocawesome101@gmail.com>
2021-03-14 15:19:22 -0400

    modify parser demo some more

--- lua_parser_demo.lua
+++ lua_parser_demo.lua
@@ -5,15 +5,17 @@ local parser = require("parser")
 local code = io.open(..., "r"):read("a")
 
 local new = parser.new(code)
-new.discard_whitespace = true
--- incomplete Lua syntax
-new:addToken("splitter", " \n,\"'.<>&^|~:-=/")
+--new.discard_whitespace = true
+--incomplete Lua syntax
+new:addToken("splitter", " \n,\"'.<>&^|~:-+%=/*")
 new:addToken("bracket", "[](){}")
+new:addToken("keyword", "function")
 new:addToken("keyword", "for")
 new:addToken("keyword", "in")
 new:addToken("keyword", "do")
 new:addToken("keyword", "if")
 new:addToken("keyword", "then")
+new:addToken("keyword", "elseif")
 new:addToken("keyword", "repeat")
 new:addToken("keyword", "until")
 new:addToken("keyword", "end")
@@ -26,16 +28,18 @@ new:addToken("constant", "nil")
 new:addToken("match", "^%d[%d%.]+$", "constant")
 new:addToken("match", "^0x[0-9a-fA-F%.]+$", "constant")
 
+-- prints sort-of-highlighted code
+-- does not parse comments or strings
 for word, ttype in function()return new:matchToken()end do
   local esc = "\27[39m"
   if ttype == "keyword" then
     esc = "\27[91m"
   elseif ttype == "constant" then
-    esc = "\27[96m"
+    esc = "\27[95m"
   elseif ttype == "splitter" then
     esc = "\27[93m"
   elseif ttype == "bracket" then
     esc = "\27[92m"
   end
-  io.write(" "..esc..--[[ttype.."::"..]]word)--print(word, ttype)
+  io.write(esc..word)--print(word, ttype)
 end
